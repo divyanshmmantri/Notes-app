41b716f01493b08a529435085cc9e2aa27b870d2
divyanshmmantri <56482570+divyanshmmantri@users.noreply.github.com>
2020-04-02 16:32:40 +0530

    Create Notes.java

---
+++ java/com.example.notesapp/Notes.java
@@ -0,0 +1,23 @@
+package com.example.notesapp;
+
+public class Notes {
+
+    private String date;
+    private String notes;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
